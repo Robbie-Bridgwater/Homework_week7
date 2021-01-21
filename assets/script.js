c3e00e409f6ac7b81223b7629d08622874dfab32
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-01-21 18:46:37 +0000

    managed to get the fiveday forecast in console and polish up the UV index by using inputted latitude and longitude

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,4 @@
 let APIKey = '157838fc226a31fe1ea09f1f52674ede';
-// let searchedCity = 'Birmingham';
-let lat = 33.5207;
-let lon = -86.8025;
 let searchButton = $('.searchButton');
 let searchInput = $(".searchInput");
 
@@ -9,44 +6,61 @@ function dynamicText(text, response, string) {
     $("." + text + " ").text(" " + string + " " + response);
 }
 
-// Searched city will go as argument below instead of global variable which is being used to test this piece of code.
 function getWeatherData(searchedCity) {
     let queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchedCity + '&appid=' + APIKey + "&units=metric";
-    var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,daily" + "&appid=" + APIKey;
     $.ajax({
         url: queryURL,
         method: 'GET'
-    }).then(function(API_data) {
-        // you can pass API_data as an argument to an external function. allowing you to reference it like API_data.main.temp
+    }).
+    then(function(API_data) {
         console.log(API_data);
-        let cityObj = {
+        var cityObj = {
             cityName: API_data.name,
             cityTemp: API_data.main.temp,
             cityHumidity: API_data.main.humidity,
             cityWindSpeed: API_data.wind.speed,
-            cityWeatherIconName: API_data.weather[0].icon
+            cityWeatherIconName: API_data.weather[0].icon,
+            cityCoord: API_data.coord
         }
         dynamicText("windSpeedDiv", cityObj.cityWindSpeed, "Wind Speed (FPM)");
         dynamicText("humidityDiv", cityObj.cityHumidity, "Humidity (RH%)");
-        dynamicText("temperatureDiv", cityObj.cityTemp, "Temperature (K)")
+        dynamicText("temperatureDiv", cityObj.cityTemp, "Temperature (°C)")
         console.log(cityObj);
-    })
-    $.ajax({
-        url: queryURL2,
-        method: 'GET'
-    }).then(function(API_data2) {
-        console.log(API_data2);
-        let cityUVIndex = API_data2.current.uvi;
-        dynamicText("UVDiv", cityUVIndex, "UV (UVI)")
-        console.log(cityUVIndex);
+        let queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityObj.cityCoord.lat + "&lon=" + cityObj.cityCoord.lon + "&exclude=hourly,daily" + "&appid=" + APIKey;
+        $.ajax({
+            url: queryURL2,
+            method: 'GET'
+        }).
+        then(function(API_data2) {
+            console.log(API_data2);
+            let cityUVIndex = API_data2.current.uvi;
+            dynamicText("UVDiv", cityUVIndex, "UV Index (UVI)")
+            console.log(cityUVIndex);
+        });
     });
+
+    function getFiveDayForecast() {
+        let queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&APPID=" + APIKey + "&units=metric";
+        $.ajax({
+            url: queryURL3,
+            method: 'GET'
+        }).
+        then(function(fiveDayForecast) {
+            console.log(fiveDayForecast);
+        })
+    };
+    getFiveDayForecast();
 };
 
-searchButton.on("click", function(e) {
-    e.preventDefault();
+
+searchButton.on("click", function(event) {
+    event.preventDefault();
     if (searchInput.val() === "") {
         alert("You must enter a city");
         return;
     }
     getWeatherData(searchInput.val());
+
 });
+
+// the next task will be to create a for Loop which gets the relevant information for the five day forecast and to make a function that creates the forecast card!
